980ed2ace7958be2ef1fcb89ebfc645a7e18ac95
SKTT1Ryze <ccc@neos.moe>
2023-11-26 15:56:19 +0900

    cdk init

---
+++ test/neos-backend-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NeosBackendInfra from '../lib/neos-backend-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/neos-backend-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NeosBackendInfra.NeosBackendInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
